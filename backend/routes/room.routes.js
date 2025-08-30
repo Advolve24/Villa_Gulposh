dd6bf92c2abdbaaee72694d75520034618edde80
Advolve24 <web@advolve.in>
2025-08-30 11:11:57 +0530

    Update room.routes.js

--- backend/routes/room.routes.js
+++ backend/routes/room.routes.js
@@ -3,11 +3,14 @@ import {
   listRooms,
   getRoomById,
   getBlockedDates,
+  getBlockedDatesAll,
 } from "../controllers/room.controller.js";
 
 const router = express.Router();
 
 router.get("/", listRooms);
+
+router.get("/blocked", getBlockedDatesAll); 
 
 router.get("/:id/blocked", getBlockedDates);
 
